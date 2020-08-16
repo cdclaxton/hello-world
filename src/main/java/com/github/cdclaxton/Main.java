d98decd6637529280b9fdcdddc083592de54cf27
cdclaxton <c.d.claxton@gmail.com>
2020-08-16 13:59:32 +0100

    Added package name.

--- src/main/java/com/github/cdclaxton/Main.java
+++ src/main/java/com/github/cdclaxton/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Package name: " + Main.class.getPackage().getName());
         System.out.println("Running version: " + Main.class.getPackage().getImplementationVersion());
     }
 }
