f43b76e1e10440bb0006d5923006c9d157e22aac
cdclaxton <c.d.claxton@gmail.com>
2020-08-21 19:09:17 +0100

    Updated message.

--- src/main/java/com/github/cdclaxton/Main.java
+++ src/main/java/com/github/cdclaxton/Main.java
@@ -3,7 +3,7 @@ package com.github.cdclaxton;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! This is a Java project for testing Jenkins.");
         System.out.println("Package name: " + Main.class.getPackage().getName());
         System.out.println("Running version: " + Main.class.getPackage().getImplementationVersion());
     }
