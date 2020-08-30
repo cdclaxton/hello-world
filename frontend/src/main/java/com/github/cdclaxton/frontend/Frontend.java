70504a6abdc5332f164dab320932ef5fdbc38c9c
cdclaxton <c.d.claxton@gmail.com>
2020-08-30 20:46:58 +0100

    Added message to standard output.

--- frontend/src/main/java/com/github/cdclaxton/frontend/Frontend.java
+++ frontend/src/main/java/com/github/cdclaxton/frontend/Frontend.java
@@ -5,12 +5,12 @@ import com.github.cdclaxton.backend.Backend;
 public class Frontend {
 
     public static void main(String[] args) {
-        String message = "";
+        System.out.println(getMessageFromBackend());
         System.out.println("Running front-end version: " + Frontend.class.getPackage().getImplementationVersion());
         System.out.println("Running back-end version: " + Backend.class.getPackage().getImplementationVersion());
     }
 
-    String getMessageFromBackend() {
+    static String getMessageFromBackend() {
         return Backend.getMessage();
     }
 
