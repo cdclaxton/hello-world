c7257807e622e90eb0a2bf6d6b1e2b3f1692b3d1
cdclaxton <c.d.claxton@gmail.com>
2020-08-30 13:23:16 +0100

    Corrected typo.

--- frontend/src/main/java/com/github/cdclaxton/frontend/Frontend.java
+++ frontend/src/main/java/com/github/cdclaxton/frontend/Frontend.java
@@ -4,7 +4,7 @@ import com.github.cdclaxton.backend.Backend;
 
 public class Frontend {
 
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
         String message = "";
         System.out.println("Running front-end version: " + Frontend.class.getPackage().getImplementationVersion());
         System.out.println("Running back-end version: " + Backend.class.getPackage().getImplementationVersion());
